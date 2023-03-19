2f0b897860e8db7f68c89f19ac1326f57d2e10dd
ashinnotfound <ashinnotfound@qq.com>
2023-03-19 20:31:56 +0800

    improvement(config):优化配置类结构

---
+++ src/main/java/com/ashin/config/QqConfig.java
@@ -0,0 +1,20 @@
+package com.ashin.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * qq配置
+ *
+ * @author ashinnotfound
+ * @date 2023/03/04
+ */
+@Data
+@Component
+@ConfigurationProperties("qq")
+public class QqConfig {
+    private Boolean enable;
+    private Long account;
+    private String password;
+}
---
+++ src/main/java/com/ashin/config/WechatConfig.java
@@ -0,0 +1,19 @@
+package com.ashin.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * 微信配置
+ *
+ * @author ashinnotfound
+ * @date 2023/03/19
+ */
+@Data
+@Component
+@ConfigurationProperties("wechat")
+public class WechatConfig {
+    private Boolean enable;
+    private String qrPath;
+}
